95d488736fd8995e2c59cb600bf2842dff4b177a
KisImre <imre.kis@lightware.eu>
2017-12-24 23:04:26 +0100

    Back to basics

--- mt7603_eeprom.c
+++ mt7603_eeprom.c
@@ -124,8 +124,6 @@ mt7603_apply_cal_free_data(struct mt7603_dev *dev, u8 *efuse)
         printk("mt7603_apply_cal_free_data eeprom[%02X] = %02X vs efuse[%02X] = %02X\n", offset, eeprom[offset], offset, efuse[offset]);
 	    eeprom[offset] = efuse[offset];
 	}
-
-    eeprom[0x56] = eeprom[0x5C] = 0xC0;    // It's magic
 }
 
 
