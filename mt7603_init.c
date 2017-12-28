9252cd1466f6f5a21b11e042609a2a0049638cb5
KisImre <imre.kis@lightware.eu>
2017-12-28 22:18:52 +0100

    is_mt7628

--- mt7603_init.c
+++ mt7603_init.c
@@ -105,7 +105,7 @@ mt7603_dma_sched_init(struct mt7603_dev *dev)
 
 	mt76_wr(dev, MT_RSV_MAX_THRESH, page_count);
 
-	if (is_mt7603(dev) && mt76xx_rev(dev) < MT7603_REV_E2) {
+	if ((is_mt7603(dev) && mt76xx_rev(dev) < MT7603_REV_E2) || is_mt7628(dev)) {
 		mt76_wr(dev, MT_GROUP_THRESH(0), page_count);
 		mt76_wr(dev, MT_BMAP_0, 0xffff);
 	} else {
